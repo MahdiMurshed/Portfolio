342fd96341957fe4e3235b8f0a855a67c8a2c67d
Mahdi_Murshed <123mahdi12345@gmail.com>
2022-05-28 01:36:25 +0600

    navbar

--- port/src/components/Navbar/Navbar.tsx
+++ port/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,31 @@
+import Image from "next/image";
 import React from "react";
-import "./Navbar.module.scss";
+import styles from "./Navbar.module.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.app__navbar}>
+      <div className={styles["app__navbar-logo"]}>
+        <div>
+          <Image
+            src={images.logo}
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+      <ul className={styles["app__navbar-links"]}>
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
