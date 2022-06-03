923cf557676b472e071a2bd5145c6d21f57d71ae
Mahdi_Murshed <123mahdi12345@gmail.com>
2022-06-04 00:26:24 +0600

    header #1

--- port/src/container/Header/Header.tsx
+++ port/src/container/Header/Header.tsx
@@ -1,8 +1,33 @@
 import React from "react";
-import "./Header.module.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={`${styles["app__header"]} app__flex`}>
+      <motion.div
+        whileInView={{ x: [-200, 0] }}
+        transition={{ duration: 1 }}
+        className={styles["app__header-info"]}
+      >
+        <div className={styles["app__header-badge"]}>
+          <div className={`${styles["badge-cmp"]} app__flex`}>
+            <span>H</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text"> HELLO, I AM </p>
+              <h1 className="head-text">MAHDI</h1>
+            </div>
+          </div>
+          <div className={`${styles["tag-cmp"]}  app__flex`}>
+            <p className="p-text"> Web Developer</p>
+            <p className="p-text"> Freelancer </p>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Header;
